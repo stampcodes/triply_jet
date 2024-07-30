4f789f2b66bb2fe61dd00dc7adf9c926c74d6cb7
Andre <andreafragnelli@gmail.com>
2024-07-30 12:19:14 +0200

    modifiche alla Hero Section

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,5 +1,17 @@
+import styles from "./Hero.module.css";
+import { FaCircleArrowDown } from "react-icons/fa6";
+
 const Hero = () => {
-  return <div>Hero</div>;
+  return (
+    <>
+      <div className={styles.hero}>
+        <div className={styles.heroContent}>
+          <h1>Scroll Down to Unlock Exclusive Deals</h1>
+          <FaCircleArrowDown className={styles.arrow} />
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Hero;
