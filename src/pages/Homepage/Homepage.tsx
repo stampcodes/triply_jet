7848ef27181becba42e902a90bf113747019413b
Andre <andreafragnelli@gmail.com>
2024-07-31 13:13:49 +0200

    eliminato scrollify

--- src/pages/Homepage/Homepage.tsx
+++ src/pages/Homepage/Homepage.tsx
@@ -2,24 +2,14 @@ import Navbar from "../../components/Navbar/Navbar";
 import Hero from "../../components/Hero/Hero";
 import Offers from "../../components/Offers/Offers";
 import BenefitsSection from "../../components/BenefitsSection/BenefitsSection";
-import useScrollify from "../../hook/useScrollify/useScrollify";
 
 const Homepage: React.FC = () => {
-  useScrollify();
   return (
     <>
-      <div className="div-scrollify">
-        <Navbar />
-      </div>
-      <div className="div-scrollify">
-        <Hero />
-      </div>
-      <div className="div-scrollify">
-        <Offers />
-      </div>
-      <div className="div-scrollify">
-        <BenefitsSection />
-      </div>
+      <Navbar />
+      <Hero />
+      <Offers />
+      <BenefitsSection />
     </>
   );
 };
