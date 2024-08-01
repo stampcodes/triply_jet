41d70e58d9aabf411e99630a9f652dac4703aeee
Andre <andreafragnelli@gmail.com>
2024-08-01 08:42:27 +0200

    aggiunto helmet provider al main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,14 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { Web3ModalProvider } from "./config/wagmi.tsx";
+import { HelmetProvider } from "react-helmet-async";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Web3ModalProvider>
-      <App />
+      <HelmetProvider>
+        <App />
+      </HelmetProvider>
     </Web3ModalProvider>
   </React.StrictMode>
 );
