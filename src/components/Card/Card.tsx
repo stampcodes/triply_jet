62993f3a807b52aff69f6681a636a20e13d13eb7
Andre <andreafragnelli@gmail.com>
2024-08-07 10:25:42 +0200

    aggiunto il click a tutta la card

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -16,16 +16,18 @@ const Card: React.FC<CardProps> = ({ card }) => {
   return (
     <>
       <article className={styles.card}>
-        <img src={card.image} alt={card.name} className={styles.cardImage} />
-        <div className={styles.rightSection}>
-          <h2 className={styles.cardName}>{card.name}</h2>
-          <p className={styles.cardDescription}>{card.description}</p>
-          <p className={styles.cardDates}>{card.travelDates}</p>
-          <p className={styles.cardPrice}>{card.price} ETH</p>
-          <button className={styles.cardButton}>
-            <Link to={`/trip/${card.id}`}>View Offer</Link>
-          </button>
-        </div>
+        <Link to={`/trip/${card.id}`} className={styles.cardLink}>
+          <img src={card.image} alt={card.name} className={styles.cardImage} />
+          <div className={styles.rightSection}>
+            <h2 className={styles.cardName}>{card.name}</h2>
+            <p className={styles.cardDescription}>{card.description}</p>
+            <p className={styles.cardDates}>{card.travelDates}</p>
+            <p className={styles.cardPrice}>{card.price} ETH</p>
+            <button className={styles.cardButton}>
+              <Link to={`/trip/${card.id}`}>View Offer</Link>
+            </button>
+          </div>
+        </Link>
       </article>
     </>
   );
